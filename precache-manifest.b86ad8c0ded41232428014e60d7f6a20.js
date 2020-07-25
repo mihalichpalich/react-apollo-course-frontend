f7ba7b002391749252ce695aca21dd56112dfab0
mihalichpalich <pavlovtfspb@gmail.com>
2020-07-26 00:26:50 +0300

    Updates

---
+++ precache-manifest.b86ad8c0ded41232428014e60d7f6a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3172643e89f965e8449851250231fba2",
+    "url": "/movies-crud/index.html"
+  },
+  {
+    "revision": "fc5a6fa8927aa39e79c3",
+    "url": "/movies-crud/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f50acc2a2b4492f27708",
+    "url": "/movies-crud/static/js/2.4deca2ff.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/movies-crud/static/js/2.4deca2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc5a6fa8927aa39e79c3",
+    "url": "/movies-crud/static/js/main.27850481.chunk.js"
+  },
+  {
+    "revision": "fb9e594707b544667a01",
+    "url": "/movies-crud/static/js/runtime-main.0a320a1f.js"
+  }
+]);
